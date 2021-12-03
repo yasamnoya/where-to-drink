69fe1cfcfb9d7c0ead8b38c4b0f3121a67febfa7
J.N. Yiunn <yasamnono@gmail.com>
2021-12-03 14:19:12 +0800

    frontend: layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav :user="user" />
     <router-view />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Nav from './components/Nav.vue';
 
 export default {
   data: () => ({
     user: null,
   }),
+  components: {
+    Nav,
+  },
   async created() {
     try {
       const res = await axios.get(`${process.env.VUE_APP_BACKEND_URL}/users/me`);
@@ -32,19 +33,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: whitesmoke;
+  background: #c33764; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #1d2671, #c33764); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to bottom,
+    #1d2671,
+    #ad5389
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  min-width: 100vw;
+  min-height: 100vh;
 }
 </style>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark w-75 mx-auto">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">今晚哪裡喝</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <router-link class="nav-link" :class="$route.path == '/' ? 'active' : ''" to="/"
+              >找地方喝！</router-link
+            >
+          </li>
+        </ul>
+        <div class="">
+          <ul class="navbar-nav mb-2 mb-md-0">
+            <li v-if="user" class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img :src="user.avatarUrl" id="avatar" alt="avatar" class="rounded-circle" />
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item" :href="`${backendUrl}/users/logout`">登出</a></li>
+              </ul>
+            </li>
+            <router-link v-if="!user" to="/login" class="btn btn-primary">登入</router-link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    user: null,
+  },
+  data: () => ({
+    backendUrl: process.env.VUE_APP_BACKEND_URL,
+  }),
+};
+</script>
+
+<style>
+#avatar {
+  width: 36px;
+  height: 36px;
+}
+</style>
