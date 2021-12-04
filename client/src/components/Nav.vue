3ee95aba8ef10ec3181634ac83c156b0fe3bd0a7
J.N. Yiunn <yasamnono@gmail.com>
2021-12-04 22:02:12 +0800

    modify: navbar

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark">
     <div class="container-fluid w-75 mx-auto">
-      <router-link class="navbar-brand" to="/">今晚哪裡喝</router-link>
+      <router-link class="navbar-brand" to="/">
+        <img src="@/assets/logo.png" class="logo" alt="Logo" />
+        今晚哪裡喝</router-link
+      >
       <button
         class="navbar-toggler"
         type="button"
@@ -14,14 +17,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :class="$route.path == '/' ? 'active' : ''" to="/"
-              >找地方喝！</router-link
-            >
-          </li>
-        </ul>
-        <div class="">
+        <div class="ms-auto">
           <ul class="navbar-nav mb-2 mb-md-0">
             <li v-if="user" class="nav-item dropdown">
               <a
@@ -33,11 +29,9 @@
                 aria-expanded="false"
               >
                 <img :src="user.avatarUrl" id="avatar" alt="avatar" class="rounded-circle" />
+                {{ user.username }}
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
                 <li><a class="dropdown-item" :href="`${backendUrl}/users/logout`">登出</a></li>
               </ul>
             </li>
@@ -80,8 +74,18 @@ nav {
   color: whitesmoke;
 }
 
+.dropdown-item:hover {
+  color: whitesmoke;
+  background-color: rgba(10, 0, 64, 1);
+}
+
 #avatar {
   width: 36px;
   height: 36px;
+}
+
+.logo {
+  width: 48px;
+  height: 48px;
 }
 </style>
