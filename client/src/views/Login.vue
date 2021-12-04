d8d6002ea219192debaf2d8c3709637e787f1f5e
J.N. Yiunn <yasamnono@gmail.com>
2021-12-04 21:37:45 +0800

    frontend: modify login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="card m-auto">
-    <div class="card-header pt-3">
-      <h2 class="card-title">登入</h2>
-    </div>
-    <div class="card-body">
-      <div class="m-1">
-        <a :href="`${backendUrl}/users/facebook`" class="btn btn-primary login-option"
-          > Facebook
-        </a>
+  <div class="d-flex flex-column p-0 wrapper">
+    <div class="card m-auto">
+      <div class="card-header pt-3">
+        <h1 class="card-title">登入</h1>
       </div>
-      <div class="m-1">
-        <a :href="`${backendUrl}/users/google`" class="btn btn-danger login-option"> Google</a>
+      <div class="card-body">
+        <div class="m-2">
+          <a :href="`${backendUrl}/users/facebook`" class="btn btn-primary login-option"
+            > Facebook
+          </a>
+        </div>
+        <div class="m-2">
+          <a :href="`${backendUrl}/users/google`" class="btn btn-danger login-option"> Google</a>
+        </div>
       </div>
     </div>
   </div>
@@ -25,11 +27,17 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  height: 60vh;
+}
+
 .card {
-  max-width: 500px;
+  background-color: rgba(16, 0, 64, 0.5);
+  width: 50vw;
+  min-width: 250px;
 }
 .login-option {
-  width: 400px;
-  font-size: 20px;
+  width: 80%;
+  font-size: 1.2em;
 }
 </style>
